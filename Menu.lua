caad070523687d2561ff5a1946d70febcd4bf439
Zayac333 <superzayac333@gmail.com>
2026-04-07 19:47:36 +0300

    Update Menu.lua

--- Menu.lua
+++ Menu.lua
@@ -1,32 +1,26 @@
+-- [[ ZAYAC HUB - CLEAN MENU ]] --
 repeat task.wait() until game:IsLoaded()
 
--- Функція для видалення чужих вікон
-local function cleanup()
-    for _, v in pairs(game:GetService("CoreGui"):GetChildren()) do
-        if v:IsA("ScreenGui") and (v.Name:find("Fire") or v:FindFirstChild("MainFrame")) then
-            v:Destroy()
-        end
+local t = (typeof(getgenv) == "function") and getgenv() or _G
+
+-- Завантажуємо бібліотеку БЕЗ помилок
+local libUrl = "https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/ParentLibrary.lua?t=" .. tick()
+local success, result = pcall(function() 
+    return loadstring(game:HttpGet(libUrl))() 
+end)
+
+if success and result then
+    local lib = result
+    
+    -- Перезаписуємо MakeWindow, щоб він працював стабільно
+    local oldMW = lib.MakeWindow
+    lib.MakeWindow = function(self, options, isHub)
+        local window = oldMW(self, options)
+        return window -- ТУТ ПОВИНЕН БУТИ RETURN
     end
+
+    t._FIRELIB = lib
+    return lib
+else
+    warn("Помилка завантаження бібліотеки: " .. tostring(result))
 end
-cleanup() -- Чистимо перед завантаженням
-
-local function getGlobalTable()
-    return typeof(getgenv) == "function" and typeof(getgenv()) == "table" and getgenv() or _G
-end
-local t = getGlobalTable()
-
--- Завантажуємо бібліотеку з ПОВНИМ ігноруванням кешу
-local libUrl = "https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/ParentLibrary.lua?nocache=" .. tick()
-local lib = loadstring(game:HttpGet(libUrl))()
-
-if lib then
-    local mw = lib.MakeWindow
-    lib.MakeWindow = function(self, options, fireHubWindow)
-        cleanup() -- Чистимо ще раз перед створенням твого вікна
-        local window = mw(self, options)
-        return window
-    end
-end
-
-t._FIRELIB = lib
-return lib
