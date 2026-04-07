ac107a1a3919d1a37186b751f741b63bf38aa397
Zayac333 <superzayac333@gmail.com>
2026-04-07 15:59:10 +0300

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,38 +1,52 @@
--- [[ ПІДКЛЮЧЕННЯ ТВОЇХ БІБЛІОТЕК ]] --
--- Використовуємо raw посилання, щоб скрипт міг зчитати чистий код
 local network = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/Librare2.lua", true))()
-local espLib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/Librare.lua", true))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/Librare.lua", true))()
 
-print("Бібліотеки Zayac333 завантажено!")
 
--- [[ 1. БЛОКУВАННЯ УРОНУ (GODMODE ВІД ОЧЕЙ) ]] --
+local vals = {
+    AntiEyefestation = true,
+    NoLocalDamage = true,
+    InstantInteract = true
+}
+
+
+local window = lib:MakeWindow({Title = "Zayac Hub - Pressure", CloseCallback = function()
+    print("Menu Closed")
+end}, true)
+
+local mainPage = window:AddPage({Title = "Main Cheats"})
+
+mainPage:AddToggle({Caption = "Anti Eyefestation (Shark)", Default = true, Callback = function(b)
+    vals.AntiEyefestation = b
+end})
+
+mainPage:AddToggle({Caption = "GodMode (No Damage)", Default = true, Callback = function(b)
+    vals.NoLocalDamage = b
+end})
+
+mainPage:AddToggle({Caption = "Instant Interact", Default = true, Callback = function(b)
+    vals.InstantInteract = b
+end})
+
+
 local oldNamecall
 oldNamecall = hookmetamethod(game, "__namecall", function(self, ...)
     local method = getnamecallmethod()
-    local args = {...}
-    
-    -- Блокуємо івент LocalDamage, щоб Eyefestation не зносила HP
-    if method == "FireServer" and self.Name == "LocalDamage" then
+    if vals.NoLocalDamage and method == "FireServer" and self.Name == "LocalDamage" then
         return nil 
     end
-    
     return oldNamecall(self, ...)
 end)
 
--- [[ 2. АВТО-ВИМКНЕННЯ EYEFESTATION ]] --
+
 task.spawn(function()
     while task.wait(0.5) do
-        -- Перевіряємо наявність папки з монстрами
-        local monstersFolder = workspace:FindFirstChild("GameplayFolder") and workspace.GameplayFolder:FindFirstChild("Monsters")
-        
-        if monstersFolder then
-            for _, monster in pairs(monstersFolder:GetChildren()) do
-                -- Шукаємо за назвою (Eye/Shark)
-                if monster.Name:lower():match("eyefestation") then
-                    local active = monster:FindFirstChild("Active")
-                    if active and active.Value == true then
-                        active.Value = false
-                        print("Eyefestation (Shark) neutralized!")
+        if vals.AntiEyefestation then
+            local monsters = workspace:FindFirstChild("GameplayFolder") and workspace.GameplayFolder:FindFirstChild("Monsters")
+            if monsters then
+                for _, monster in pairs(monsters:GetChildren()) do
+                    if monster.Name:lower():match("eyefestation") then
+                        local active = monster:FindFirstChild("Active")
+                        if active then active.Value = false end
                     end
                 end
             end
@@ -40,22 +54,9 @@ task.spawn(function()
     end
 end)
 
--- [[ 3. ОСНОВНА ЛОГІКА ТА ESP ]] --
-local vals = {
-    AntiEyefestation = true,
-    InstantInteract = true
-}
 
--- Швидка взаємодія через твій network
 game:GetService("ProximityPromptService").PromptButtonHoldBegan:Connect(function(prompt)
     if vals.InstantInteract then
-        -- Викликаємо функцію кліку з бібліотеки
-        if network and network.Other then
-            network.Other:FireProximityPrompt(prompt)
-        else
-            fireproximityprompt(prompt)
-        end
+        fireproximityprompt(prompt)
     end
 end)
-
-print("--- Anti-Eyefestation Shield & Libraries Active ---")
