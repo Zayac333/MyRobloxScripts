c0e4d5f19ed6658c046f7e3c6cf6605549f648df
Zayac333 <superzayac333@gmail.com>
2026-05-02 16:25:01 +0300

    Update ZayacHubLoader.lua

--- ZayacHubLoader.lua
+++ ZayacHubLoader.lua
@@ -1,38 +1,39 @@
 local PlaceId = game.PlaceId
 
--- Список ігор для Pressure (Давление)
 local PressureGames = {
-    [12552538292] = true, -- Основна гра
-    [12411473842] = true, -- Лобі
-    [12552511828] = true, -- Операція Heartbeat
-    [124505452456262] = true -- Інше
+    [12552538292] = true,
+    [12411473842] = true, 
+    [12552511828] = true,
+    [124505452456262] = true 
 }
 
--- Список ігор для TSB (The Strongest Battlegrounds)
 local TSBGames = {
-    [10449761463] = true, -- ТСБ основний
-    [12360882630] = true, -- ТСБ дуелі
-    [131048399685555] = true -- ТСБ ВС
+    [10449761463] = true,
+    [12360882630] = true, 
+    [131048399685555] = true 
 }
 
--- Список ігор для JJS (Jujutsu Shenanigans)
 local JJSGames = {
     [9391468976] = true -- ЖЖС
 }
 
+local RakeGames = {
+    [2413927524]
+    [8795690372]
+}
+
 if PressureGames[PlaceId] then
-    -- Завантаження для Pressure
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/Pressure/main/ZayacHub.lua", true))()
     
 elseif TSBGames[PlaceId] then
-    -- Завантаження для TSB
     loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/ZayacHubEsp.lua", true))()
 
 elseif JJSGames[PlaceId] then
-    -- Завантаження для ЖЖС (нове посилання)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/MiLiteHub2.lua", true))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/MyRobloxScripts/main/ZayacJjs.lua", true))()
+    
+elseif RakeGames[PlaceId] then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Zayac333/TheRake/main/ZayacHub.lua", true))()
     
 else
-    -- Якщо гри немає в списку
     warn("ZayacHub: Game not supported. ID: " .. PlaceId)
 end
